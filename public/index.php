93d2cd6325cdd7609e78be7c742742b4033900b9
myklv <+@myklv.com>
2018-11-26 13:40:40 -0500

    move file listing handler to it's own file

--- public/index.php
+++ public/index.php
@@ -13,24 +13,5 @@ case "/c":
     echo "$route to be implemented";
     break;
 default:
-    $is_dir = is_dir("$dir$target/");
-    if ($target && !$is_dir) {
-        echo "not able to display file <address>$target</address>";
-        echo "<a href=../" . dirname($target) . "/>..</a>";
-        break;
-    }
-    if ($target && $is_dir) {
-        $dir = "$dir$target/";
-        echo "<img src={$prefix}dir.gif width=18> ";
-        echo "<a href=../>..</a><br>\n";
-    }
-    $files = glob("$dir*");
-    foreach ($files as $f) {
-        $filesize = floor(filesize($f)/1024);
-        $icon = $prefix . (is_dir($f) ? 'dir.gif' : 'file.gif');
-        if (is_dir($f)) $f .= "/";
-        $f = substr($f,strlen($dir));
-        echo "<img src=$icon width=18> ";
-        echo "<a href=$f title={$filesize}kb>$f</a><br>\n";
-    }
+    require "../src/handlers/list.php";
 }
