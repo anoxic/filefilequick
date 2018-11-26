52c4cbb564d540d1847fbfd1c6ab0d17c877f159
myklv <+@myklv.com>
2018-11-26 13:14:13 -0500

    a funky file listing file

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+$dir = getenv('FILE_DIR');
+$prefix = getenv('PREFIX');
+$uri = $_SERVER['REQUEST_URI'];
+
+list($route, $target) = dispatch(seq('/c','/d'), $uri, $prefix);
+
+switch ($route) {
+case "/d":
+case "/c":
+    echo "$route to be implemented";
+    break;
+default:
+    if ($target && is_dir($d="$dir$target/")) {
+        $dir = "$dir$target/";
+        echo "<img src={$prefix}dir.gif width=18> ";
+        echo "<a href=../>..</a><br>\n";
+    }
+    $files = glob("$dir*");
+    foreach ($files as $f) {
+        $filesize = floor(filesize($f)/1024);
+        $icon = $prefix . (is_dir($f) ? 'dir.gif' : 'file.gif');
+        if (is_dir($f)) $f .= "/";
+        $f = substr($f,strlen($dir));
+        echo "<img src=$icon width=18> ";
+        echo "<a href=$f title={$filesize}kb>$f</a><br>\n";
+    }
+}
+
+
+function dispatch($routes, $uri, $prefix)
+{
+    foreach ($routes as $r) {
+        if (fnmatch("$prefix*$r", $uri)) break;
+        $r = false;
+    }
+    return [$r, substr($uri, strlen($prefix),
+        $routes->valid() ? -strlen($r) : strlen($uri))];
+}
+
+function seq()
+{
+    $i = 0;
+    while ($a = func_get_arg($i++)) {
+        yield $a;
+    }
+}
